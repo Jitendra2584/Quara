dc7fa52ac78cbdee9b29f26e391c476c09ef0bfa
Jitendra <jitendrasingh28321155@gmail.com>
2023-07-23 16:36:22 +0530

    project push

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Question,Like,Answer
 
+
+admin.site.register(Question)
+admin.site.register(Like)
+admin.site.register(Answer)
 # Register your models here.
